aaba2be097605f41b26184ca100bac26931f8880
eshikaz <kazuki.eshi@japan-systems.co.jp>
2023-07-11 16:23:50 +0900

    7/11

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,30 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [text, setText] = useState("");
-  const [addText, setAddText] = useState("");
+  const [text, setText] = useState('');
+  const [todoList, setTodoList] = useState<string[]>([]);
+
   const onAddText = () => {
-    setAddText(text);
-    setText("");
-  }
+    setTodoList([...todoList, text]);
+    setText('');
+  };
+
   return (
     <div className="App">
       <input
-      placeholder="ここに書け"
+        placeholder="ここに入力"
         value={text}
         onChange={(event) => setText(event.target.value)}
       />
       <button onClick={onAddText}>追加</button>
-      <p>{addText}</p>
+      <ul>
+        {todoList.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default App;
+
