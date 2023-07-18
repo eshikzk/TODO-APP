a334ae97b7b9178d4a4cf2284f40191c2cfc6896
eshikaz <kazuki.eshi@japan-systems.co.jp>
2023-07-18 21:09:27 +0900

    7/18

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import {
+  Container,
   Button,
-  TextField
-}from "@mui/material";
+  TextField,
+  Typography,
+  Box,
+  List,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 
 const App = () => {
   const [text, setText] = useState('');
@@ -14,22 +20,52 @@ const App = () => {
     setText('');
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onAddText();
+  };
+
+  const handleTodoClick = (todo: string) => {
+    alert(todo);
+  };
+
   return (
     <div className="App">
-      <input
-        placeholder="ここに入力"
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      />
-      <button onClick={onAddText}>追加</button>
-      <ul>
-        {todoList.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
+      <Container component="main" maxWidth="xs">
+        <Typography
+          sx={{ mt: 4, textAlign: "center" }}
+          component="h1"
+          variant="h5"
+        >
+          Todoリスト
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            placeholder="ここに入力"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            size="large"
+            variant="contained"
+            sx={{ my: 0 }}
+          >
+            追加
+          </Button>
+        </Box>
+        <List>
+          {todoList.map((todo, index) => (
+            <ListItemButton key={index} onClick={() => handleTodoClick(todo)}>
+              <ListItemText primary={todo} />
+            </ListItemButton>
+          ))}
+        </List>
+      </Container>
     </div>
   );
 };
 
 export default App;
-
